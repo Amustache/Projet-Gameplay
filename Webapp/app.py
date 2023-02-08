4b223468d5a03df9c112831c26bfc4af808c1e2c
Corentin Junod <corentin.junod@outlook.com>
2023-02-08 01:29:21 +0100

    Moved ML, bind ML to Webapp, handle having no truth in app

--- Webapp/app.py
+++ Webapp/app.py
@@ -3,85 +3,88 @@ import io
 import logging
 import os
 import shutil
-
+import ML.model
 
 from datascience import *
 from flask import flash, Flask, redirect, render_template, request, Response, url_for
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from werkzeug.utils import secure_filename
 
-
 ALLOWED_EXTENSIONS = {"mkv", "mp4", "webm", "avi"}
-
-app = Flask(__name__)
-app.config["UPLOAD_FOLDER"] = os.path.join("static", "inputs")
-app.config["MAX_CONTENT_PATH"] = 200_000_000
-app.config.from_object("config")
-
 DEMO_TRUTH = "truth.csv"
+DEMO_FNAME = "video.webm"
 
+def root_path_join(*args):
+    return os.path.join(app.root_path, *args)
+
+def clean_upload_folder():
+    for f in os.listdir(app.config["UPLOAD_FOLDER"]):
+        full_path = root_path_join(app.config["UPLOAD_FOLDER"], f)
+        if os.path.isfile(full_path) and f != ".gitkeep":
+            os.remove(full_path)
+
+def flash_and_redirect(dest, msg):
+    flash(msg)
+    return redirect(url_for(dest))
 
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
+app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = os.path.join("static", "inputs")
+app.config["DEMO_FOLDER"]   = os.path.join("static", "demo")
+app.config["MAX_CONTENT_PATH"] = 200_000_000
+app.config.from_object("config")
 
 @app.route("/uploader", methods=["GET", "POST"])
 def upload_file():
     if request.method == "POST":
         if "file" not in request.files:
-            flash("Pas de fichier téléversé")
-            return redirect(url_for("experience"))
+            return flash_and_redirect("experience", "Pas de fichier téléversé")
+
         file = request.files["file"]
         if file.filename == "":
-            flash("Pas de fichier sélectionné")
-            return redirect(url_for("experience"))
-        if file and allowed_file(file.filename):
-            # Delete all files in the directory
-            for f in os.listdir(os.path.join(app.root_path, app.config["UPLOAD_FOLDER"])):
-                if f != ".gitkeep":
-                    os.remove(os.path.join(app.root_path, app.config["UPLOAD_FOLDER"], f))
+            return flash_and_redirect("experience", "Pas de fichier sélectionné")
 
-            # Save the new file
-            fname = secure_filename(file.filename)
-            file.save(os.path.join(app.root_path, app.config["UPLOAD_FOLDER"], fname))
+        if not (file and allowed_file(file.filename)):
+            return flash_and_redirect("experience", "Merci d'utiliser une vidéo valide")
+            
+        clean_upload_folder()
+        fname = secure_filename(file.filename)
+        full_path = root_path_join(app.config["UPLOAD_FOLDER"], fname)
 
-            # Process the video and extract the data
-            print("TODO")  # TODO
+        # Save the new file
+        file.save(full_path)
 
-            # Show the result
-            return redirect(url_for("experience_show", filename=fname))
-        flash("Merci d'utiliser une vidéo valide")
-        return redirect(url_for("experience"))
+        # Process the video and extract the data
+        ML.model.predict(full_path)
+
+        # Show the result
+        return redirect(url_for("experience_show", filename=fname))
 
 
 @app.route("/example")
 def example():
-    # Delete all files in the directory
-    for f in os.listdir(os.path.join(app.root_path, app.config["UPLOAD_FOLDER"])):
-        if f != ".gitkeep":
-            os.remove(os.path.join(app.root_path, app.config["UPLOAD_FOLDER"], f))
+    clean_upload_folder()
 
     # Copy example data
-    for f in os.listdir(os.path.join(app.root_path, "demo_inputs")):
+    for f in os.listdir(app.config["DEMO_FOLDER"]):
         if f != ".gitignore":
             shutil.copyfile(
-                os.path.join(app.root_path, "demo_inputs", f),
-                os.path.join(app.root_path, app.config["UPLOAD_FOLDER"], f),
+                root_path_join(app.config["DEMO_FOLDER"], f),
+                root_path_join(app.config["UPLOAD_FOLDER"], f)
             )
-
-    fname = "video.webm"
-
-    return redirect(url_for("experience_show", filename=fname))
+    return redirect(url_for("experience_show", filename=DEMO_FNAME))
 
 
 @app.route("/experience")
 def experience():
     return render_template("pages/experience.html")
 
-
+"""
 @app.route("/barcode.png")
 def barcode():
-    file = os.path.join(app.root_path, app.config["UPLOAD_FOLDER"], DEMO_TRUTH)
+    file = root_path_join(app.config["UPLOAD_FOLDER"], DEMO_TRUTH)
     df_trim = extract_keys_from_file(file)
     data = extract_timeline_from_df(df_trim)
 
@@ -93,7 +96,7 @@ def barcode():
 
 @app.route("/tenpatterns.png")
 def tenpatterns():
-    file = os.path.join(app.root_path, app.config["UPLOAD_FOLDER"], DEMO_TRUTH)
+    file = root_path_join(app.config["UPLOAD_FOLDER"], DEMO_TRUTH)
     df_trim = extract_keys_from_file(file)
     all_keys = extract_pattern_from_df(df_trim)
     all_patterns = find_all_patterns(all_keys)
@@ -102,11 +105,11 @@ def tenpatterns():
     output = io.BytesIO()
     FigureCanvas(fig).print_png(output)
     return Response(output.getvalue(), mimetype="image/png")
-
+"""
 
 @app.route("/markov.png")
 def markov():
-    file = os.path.join(app.root_path, app.config["UPLOAD_FOLDER"], DEMO_TRUTH)
+    file = os.path.join(app.config["UPLOAD_FOLDER"], DEMO_TRUTH)
     df_trim = extract_keys_from_file(file)
     data = extract_timeline_from_df(df_trim)
 
@@ -119,43 +122,35 @@ def markov():
 @app.route("/experience-show")
 def experience_show():
     if not request.args.get("filename"):
-        flash("Il faut d'abord téléverser une vidéo !")
-        return redirect(url_for("experience"))
+        return flash_and_redirect("experience", "Il faut d'abord téléverser une vidéo !")
+        
     filename = os.path.join(app.config["UPLOAD_FOLDER"], request.args.get("filename"))
-    if not os.path.isfile(os.path.join(app.root_path, filename)):
-        flash("Le ficiher sélectionné semble invalide")
-        return redirect(url_for("experience"))
+    if not os.path.isfile(filename):
+        return flash_and_redirect("experience", "Le fichier sélectionné semble invalide")
 
     return render_template("pages/experience-show.html", filename=filename)
-
 
 @app.route("/")
 def home():
     return render_template("pages/home.html")
 
-
 @app.route("/rapport")
 def rapport():
     return render_template("pages/rapport.html")
-
 
 @app.route("/resultats")
 def resultats():
     return render_template("pages/resultats.html")
 
-
 # Error handlers.
-
 
 @app.errorhandler(500)
 def internal_error(error):
     return render_template("errors/500.html"), 500
 
-
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template("errors/404.html"), 404
-
 
 if not app.debug:
     file_handler = FileHandler("error.log")
