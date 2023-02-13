46e6d2c69c6c2ed2bf1f18282f2c96674356f153
Corentin Junod <corentin.junod@outlook.com>
2023-02-13 17:13:59 +0100

    Re-added demo and ML

---
+++ Webapp/ML/lib/KeyslogReader.py
@@ -0,0 +1,43 @@
+import torch
+import pandas
+
+class KeyslogReader():
+    def __init__(self, keys_path, device, frame_step=1, start_frame=0, offset=0):
+        self.data = []
+
+        csv_file  = pandas.read_csv(keys_path)
+
+        print(f"Loading keys ({len(csv_file)} entries)...")
+
+        current_state = set()
+        current_frame = start_frame
+        for i in range(len(csv_file)):
+
+            frame, key, state = csv_file.loc[i]
+
+            while current_frame < frame+offset:
+                self.data.append(self.keysToArray(current_state))
+                current_frame += frame_step
+
+            if state == "UP" and key in current_state: 
+                current_state.remove(key)
+            elif state == "DOWN":
+                current_state.add(key)
+
+        self.data = torch.Tensor(self.data).to(device)
+        
+        print(f"Keys loaded. Nb of output keys : {len(self.data)}")
+    
+    def __getitem__(self, i): 
+        if i < len(self.data):
+            return self.data[i]
+        else:
+            return self.data[len(self.data)-1]
+
+    def keysToArray(self, keys) :
+        output = [0,0,0]
+        if "Key.left"  in keys : output[0] = 1
+        if "Key.right" in keys : output[1] = 1
+        if "'x'"       in keys : output[2] = 1
+        return output
+        
---
+++ Webapp/ML/lib/NeuralNetwork.py
@@ -0,0 +1,227 @@
+import torch
+import torchvision
+import cv2
+import copy
+import numpy as np
+import time
+
+from torch import nn
+
+class NeuralNetwork(nn.Module):
+    def __init__(self, device, dimensions, batch_size):
+        super(NeuralNetwork, self).__init__()
+
+        self.LEARNING_RATE = 0.0005
+        self.WEIGHT_DECAY  = 0.00001
+        self.DROPOUT = 0.5
+
+        self.input_width, self.input_height = dimensions
+        self.device = device
+        self.loss_fn = nn.BCELoss()
+
+        self.all_correct = torch.Tensor([True, True, True]).to(device)
+
+        conv_output_width = 5
+        conv_output_height = 2
+
+        self.block_init= nn.Sequential(
+            nn.Conv2d(34, 70, 7, padding=3),
+            nn.MaxPool2d(4),
+        )
+        self.block_init.apply(self.init_weights)
+
+        self.block_1 = nn.Sequential(
+            nn.Conv2d(70, 35, 3, padding=1),
+            nn.BatchNorm2d(35),
+            nn.LeakyReLU(),
+
+            nn.Conv2d(35, 35, 3, padding=1),
+            nn.LeakyReLU(),
+            nn.Dropout2d(self.DROPOUT),
+
+            nn.Conv2d(35, 70, 3, padding=1),
+            nn.BatchNorm2d(70),
+            nn.LeakyReLU(),
+            nn.Dropout2d(self.DROPOUT),
+        )
+        self.block_1.apply(self.init_weights)
+
+
+        self.block_2 = nn.Sequential(
+            nn.Conv2d(70, 35, 3, padding=1),
+            nn.BatchNorm2d(35),
+            nn.LeakyReLU(),
+
+            nn.Conv2d(35, 35, 3, padding=1),
+            nn.LeakyReLU(),
+            nn.Dropout2d(self.DROPOUT),
+
+            nn.Conv2d(35, 70, 3, padding=1),
+            nn.BatchNorm2d(70),
+            nn.LeakyReLU(),
+            nn.Dropout2d(self.DROPOUT),
+        )
+        self.block_2.apply(self.init_weights)
+
+
+        self.block_3 = nn.Sequential(
+            nn.Conv2d(70, 35, 3, padding=1),
+            nn.BatchNorm2d(35),
+            nn.LeakyReLU(),
+
+            nn.Conv2d(35, 35, 3, padding=1),
+            nn.LeakyReLU(),
+            nn.Dropout2d(self.DROPOUT),
+
+            nn.Conv2d(35, 70, 3, padding=1),
+            nn.BatchNorm2d(70),
+            nn.LeakyReLU(),
+            nn.Dropout2d(self.DROPOUT),
+        )
+        self.block_3.apply(self.init_weights)
+
+        self.block_inter = nn.Sequential(
+            nn.Conv2d(70, 100, 3, padding=1),
+            nn.Dropout2d(self.DROPOUT),
+            nn.MaxPool2d(4),
+        )
+        self.block_inter.apply(self.init_weights)
+
+
+        self.block_4 = nn.Sequential(
+            nn.Conv2d(100, 50, 3, padding=1),
+            nn.BatchNorm2d(50),
+            nn.LeakyReLU(),
+
+            nn.Conv2d(50, 50, 3, padding=1),
+            nn.LeakyReLU(),
+            nn.Dropout2d(self.DROPOUT),
+
+            nn.Conv2d(50, 100, 3, padding=1),
+            nn.BatchNorm2d(100),
+            nn.LeakyReLU(),
+            nn.Dropout2d(self.DROPOUT),
+        )
+        self.block_4.apply(self.init_weights)
+
+        self.block_5 = nn.Sequential(
+            nn.Conv2d(100, 50, 3, padding=1),
+            nn.BatchNorm2d(50),
+            nn.LeakyReLU(),
+
+            nn.Conv2d(50, 50, 3, padding=1),
+            nn.LeakyReLU(),
+            nn.Dropout2d(self.DROPOUT),
+
+            nn.Conv2d(50, 100, 3, padding=1),
+            nn.BatchNorm2d(100),
+            nn.LeakyReLU(),
+            nn.Dropout2d(self.DROPOUT),
+        )
+        self.block_5.apply(self.init_weights)
+
+
+        self.block_end = nn.Sequential(
+            nn.Conv2d(100, 130, 3, padding=1),
+            nn.Dropout2d(self.DROPOUT),
+
+            nn.MaxPool2d(3),
+
+            nn.Conv2d(130, 180, 3, padding=1),
+            nn.LeakyReLU(),
+            nn.Dropout2d(self.DROPOUT),
+
+            nn.Flatten(),
+            nn.Linear(180*conv_output_width*conv_output_height, 180),
+            nn.LeakyReLU(),
+            nn.Dropout2d(self.DROPOUT/1.5),
+            nn.Linear(180, 3),
+            nn.Sigmoid()
+        )
+        self.block_end.apply(self.init_weights)
+
+        self.optimizer = torch.optim.Adam(self.parameters(), lr=self.LEARNING_RATE, weight_decay=self.WEIGHT_DECAY)
+        
+        self.to(device)
+
+    def init_weights(self, layer):
+        if isinstance(layer, nn.Linear):
+            torch.nn.init.xavier_uniform_(layer.weight)
+            layer.bias.data.fill_(0.01)
+        if isinstance(layer, nn.Conv2d):
+            torch.nn.init.xavier_uniform_(layer.weight)
+
+
+    def forward(self, x):
+        
+        #Print the shape at each layer
+        """
+        x   = self.block_init(x)
+        x_1 = self.block_1(x)+x
+        x_2 = self.block_2(x_1)+x_1
+        x_3 = self.block_3(x_2)+x_2
+
+        x_4 = self.block_inter(x_3)
+        x_5 = self.block_4(x_4)+x_4
+        x_6 = self.block_5(x_5)+x_5
+        for layer in self.block_end:
+            print(x_6.shape)
+            x_6 = layer(x_6 )
+        return x_6
+        """
+        
+
+        x   = self.block_init(x)
+        x_1 = self.block_1(x)+x
+        x_2 = self.block_2(x_1)+x_1
+        x_3 = self.block_3(x_2)+x_2
+
+        x_4 = self.block_inter(x_3)
+        x_5 = self.block_4(x_4)+x_4
+        x_6 = self.block_5(x_5)+x_5
+
+        return self.block_end(x_6)
+
+
+    def process(self, data, is_train):
+        loss_sum      = 0
+        fully_correct = 0
+        part_correct  = torch.FloatTensor([0,0,0]).to(self.device)
+
+        nb_elem_per_x = 1 if is_train else 1
+        data_size = len(data)*nb_elem_per_x
+
+        self.train() if is_train else self.eval()
+        with torch.set_grad_enabled(is_train):
+            for batch, (x, y) in enumerate(data):
+                x = x.to(self.device)
+
+                # Forward
+                pred = self(x)
+                loss = self.loss_fn(pred, y)
+                loss_sum += loss.item()
+
+                #  Backward
+                if is_train:
+                    self.optimizer.zero_grad()
+                    loss.backward()
+                    self.optimizer.step()
+
+                corrects = torch.round(pred) == y
+                part_correct[0] += corrects[:,0].sum()
+                part_correct[1] += corrects[:,1].sum()
+                part_correct[2] += corrects[:,2].sum()
+                fully_correct += torch.logical_and( torch.logical_and(corrects[:,0], corrects[:,1]), corrects[:,2]).sum()
+
+                if is_train and batch % 50 == 0:
+                    print(f"loss:{loss.item():>3f} [{batch:>4d}/{data_size:>4d}] {100*batch/data_size:.1f}%")
+        
+        return loss_sum/data_size, part_correct/(len(data.dataset)*nb_elem_per_x), fully_correct/(len(data.dataset)*nb_elem_per_x)
+
+
+    def debug_frame(self, x, y=None, name=""):
+        print("DEBUG | X shape : ",x.shape)
+        for i in range(5):
+            for j in range(5):
+                #if y != None : print(y[i])
+                cv2.imwrite('output/image_'+name+str(i)+str(j)+".png", x[i][j].cpu().detach().numpy()*255)
