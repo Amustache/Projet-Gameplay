6b860882de02e1693bd6d8f007827d67eb878e21
Corentin Junod <corentin.junod@outlook.com>
2023-02-13 16:50:47 +0100

    Moved demo and clean

--- Webapp/ML/model.py
+++ Webapp/ML/model.py
@@ -1,27 +1,33 @@
-from datetime import datetime
-import os
 import sys
-
-
-from .inc.KeyslogReader import KeyslogReader
-from .inc.NeuralNetwork import NeuralNetwork
-from .inc.VideoLoader import VideoLoader
-from sklearn.model_selection import KFold
-from torch.utils.data import DataLoader, Subset
-from torch.utils.tensorboard import SummaryWriter
+import os
 import torch
+import numpy as np
 
+from datetime import datetime
+from torch.utils.data import DataLoader, Subset
+from torchinfo import summary
+from sklearn.model_selection import KFold
 
-DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"
-VIDEO_DIMENSIONS = (int(1920 / 8), int(1080 / 8))
-START_FRAME = 1000
-END_FRAME = 0
-OFFSET = 3
-BATCH_SIZE = 40
-NB_EPOCHS = 100
-NB_K_SPLIT = 8
+from .lib.NeuralNetwork import NeuralNetwork
+from .lib.VideoLoader import VideoLoader
+from .lib.KeyslogReader import KeyslogReader
+
+from torch.utils.tensorboard import SummaryWriter
+
+DEVICE           = "cuda:0" if torch.cuda.is_available() else "cpu"
+VIDEO_DIMENSIONS = (int(1920/8), int(1080/8))
+START_FRAME      = 1000
+END_FRAME        = 0
+OFFSET           = 3
+BATCH_SIZE       = 32
+NB_EPOCHS        = 1000
+NB_K_SPLIT       = 8
 SHUFFLE_DATASETS = False
-FPS = 25
+FPS              = 25
+NB_DIMENSIONS    = 34
+
+torch.manual_seed(3543)
+np.random.seed(4231)
 
 
 class VideoKeysLogMerge(torch.utils.data.IterableDataset):
@@ -29,20 +35,13 @@ class VideoKeysLogMerge(torch.utils.data.IterableDataset):
         self.video = VideoLoader
         self.keys = KeyslogReader
         self.next_element = 0
-
-    def __len__(self):
-        return len(self.video)
-
-    def __iter__(self):
-        return self
-
-    def __getitem__(self, i):
-        return self.video[i], self.keys[i]
-
+    def __len__(self):        return len(self.video)
+    def __iter__(self):       return self
+    def __getitem__(self, i): return self.video[i], self.keys[i]
     def __next__(self):
         if self.next_element < self.__len__():
             self.next_element += 1
-            return self.__getitem__(self.next_element - 1)
+            return self.__getitem__(self.next_element-1)
         else:
             self.next_element = 0
             raise StopIteration
@@ -54,24 +53,24 @@ def start_tensorboard():
 def write_tensorboard(name, epoch, results):
     global tensorboardWriter
     loss, correct_parts, correct = results
-    tensorboardWriter.add_scalar(name + "/Accuracy/buttonLeft", correct_parts[0], epoch)
-    tensorboardWriter.add_scalar(name + "/Accuracy/buttonRight", correct_parts[1], epoch)
-    tensorboardWriter.add_scalar(name + "/Accuracy/buttonJump", correct_parts[2], epoch)
-    tensorboardWriter.add_scalar(name + "/Accuracy", correct, epoch)
-    tensorboardWriter.add_scalar(name + "/Loss", loss, epoch)
+    tensorboardWriter.add_scalar(name+"/Accuracy/buttonLeft",  correct_parts[0], epoch)
+    tensorboardWriter.add_scalar(name+"/Accuracy/buttonRight", correct_parts[1], epoch)
+    tensorboardWriter.add_scalar(name+"/Accuracy/buttonJump",  correct_parts[2], epoch)
+    tensorboardWriter.add_scalar(name+"/Accuracy",             correct, epoch)
+    tensorboardWriter.add_scalar(name+"/Loss",                 loss, epoch)
     tensorboardWriter.flush()
 
 def save_model(model, suffix):
-    torch.save(model.state_dict(), datetime.now().strftime("%Y-%m-%d-%H:%M") + suffix)
+    torch.save(model.state_dict(), "models/"+datetime.now().strftime("%Y-%m-%d-%H:%M")+suffix)
 
 def get_video(path, preload):
     return VideoLoader(path, VIDEO_DIMENSIONS, START_FRAME, END_FRAME, FPS, preload)
 
 def get_keylog(path, frameStep):
-    return KeyslogReader(path, frameStep, START_FRAME, offset=OFFSET)
+    return KeyslogReader(path, DEVICE, frameStep, START_FRAME, offset=OFFSET)
 
 def get_model():
-    return NeuralNetwork(DEVICE, VIDEO_DIMENSIONS)
+    return NeuralNetwork(DEVICE, VIDEO_DIMENSIONS, BATCH_SIZE)
 
 def get_dataloader(data):
     return DataLoader(data, batch_size=BATCH_SIZE, shuffle=SHUFFLE_DATASETS)
@@ -83,28 +82,47 @@ def test(video_path, keylog_path):
     print("Start testing...")
     start_tensorboard()
     data_frames = get_video(video_path, preload=True)
-    data_keys = get_keylog(keylog_path, data_frames.getFrameStep())
-    data = VideoKeysLogMerge(data_frames, data_keys)
+    data_keys   = get_keylog(keylog_path, data_frames.getFrameStep())
+    data        = VideoKeysLogMerge(data_frames, data_keys)
 
     kf = KFold(n_splits=NB_K_SPLIT).split(data)
     for split, (train_indexes, test_indexes) in enumerate(kf):
 
-        data_train = get_dataloader(Subset(data, train_indexes))
-        data_test = get_dataloader(Subset(data, test_indexes))
         model = get_model()
+        txt = ""
+        txt += "Frames : "        + str(START_FRAME) + " to " + str(END_FRAME) + "  \n"
+        txt += "Batch size : "    + str(BATCH_SIZE) + "  \n"
+        txt += "Learning rate : " + str(model.LEARNING_RATE) + "  \n"
+        txt += "Weight decay : "  + str(model.WEIGHT_DECAY) + "  \n"
+        txt += "Dropout : "  + str(model.DROPOUT) + "  \n"
+        txt += str(summary(model, input_size=(BATCH_SIZE, NB_DIMENSIONS, VIDEO_DIMENSIONS[0], VIDEO_DIMENSIONS[1]), verbose=0)).replace("\n", "  \n")
+        tensorboardWriter.add_text("Parameters", txt)
+
+        data_train = get_dataloader(Subset(data, train_indexes))
+        data_test  = get_dataloader(Subset(data, test_indexes))
 
         for epoch in range(NB_EPOCHS):
             print_epoch(epoch)
             results = model.process(data_train, is_train=True)
-            write_tensorboard("Train", epoch + split * NB_EPOCHS, results)
+            loss_train, _, _ = results
+            write_tensorboard("Train", epoch+split*NB_EPOCHS, results)
             results = model.process(data_test, is_train=False)
-            loss, correct_parts, correct = results
-            print(
-                f"Test : Accuracy:{(100*correct_parts[0]):>0.1f}%|{(100*correct_parts[1]):>0.1f}%|{(100*correct_parts[2]):>0.1f}% Tot: {(100*correct):>0.1f}% Loss: {loss:>6f} \n"
-            )
-            write_tensorboard("Test", epoch + split * NB_EPOCHS, results)
+            loss_test, correct_parts_test, correct_test = results
+            print(f"Test : Accuracy:{(100*correct_parts_test[0]):>0.1f}%|{(100*correct_parts_test[1]):>0.1f}%|{(100*correct_parts_test[2]):>0.1f}% Tot: {(100*correct_test):>0.1f}% Loss: {loss_test:>6f} \n")
+            write_tensorboard("Test", epoch+split*NB_EPOCHS, results)
+            tensorboardWriter.add_scalar("Loss difference",  loss_test-loss_train, epoch)
 
-        save_model(model, "_test_model_tmp.pth")
+            #print(model.init1[0].weight)
+            
+            for i in range(len(model.block_init[0].weight)):
+                for j in range(len(model.block_init[0].weight[i])):
+                    image = torch.clone(model.block_init[0].weight.data[None,i,j])
+                    image -= torch.min(image)
+                    image /= torch.max(image)
+                    tensorboardWriter.add_image("Test/Layers_init1_"+str(i)+"_"+str(j), image, epoch*NB_EPOCHS)
+            
+
+            save_model(model, "_test_model_epoch_"+str(epoch)+".pth")
 
     save_model(model, "_test_model_finished.pth")
     print("Test finished")
@@ -114,12 +132,12 @@ def train(video_path, keylog_path):
     print("Start training...")
     start_tensorboard()
     data_frames = get_video(video_path, preload=False)
-    data_keys = get_keylog(keylog_path, data_frames.getFrameStep())
-    data = get_dataloader(VideoKeysLogMerge(data_frames, data_keys))
-    model = get_model()
+    data_keys   = get_keylog(keylog_path, data_frames.getFrameStep())
+    data        = get_dataloader(VideoKeysLogMerge(data_frames, data_keys))
+    model       = get_model()
     for epoch in range(NB_EPOCHS):
         print_epoch(epoch)
-        write_tensorboard("Train", epoch * NB_EPOCHS, model.process(data, is_train=True))
+        write_tensorboard("Train", epoch*NB_EPOCHS, model.process(data, is_train=True))
     save_model(model, "_train_model.pth")
     print("Train finished")
 
@@ -148,9 +166,7 @@ def predict(video_path, model_path=None):
             i = 0
             for x in dataloader:
                 print(f"{i}/{len(dataloader)}")
-                x[0] = x[0].to(DEVICE)
-                x[1] = x[1].to(DEVICE)
-                pred_batch = torch.round(model(x))
+                pred_batch = torch.round(model(x.to(DEVICE)))
                 for pred in pred_batch:
                     for j in range(len(current_state)):
                         if current_state[j] != pred[j]:
@@ -168,6 +184,7 @@ def predict(video_path, model_path=None):
             predict(video_path + f, model_path)
     else:
         print("Path is not a file or a directory")
+
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
---
+++ Webapp/ML/olds/inc/olds/VideoLoader.py
@@ -0,0 +1,122 @@
+import cv2
+import numpy as np
+import torch
+
+
+class VideoLoader:
+    def __init__(self, video_path, dimensions, start_frame=0, end_frame=0, fps=30, preload=False):
+        self.video = cv2.VideoCapture(video_path)
+        self.output_width, self.output_height = dimensions
+        self.start_frame = start_frame
+        self.end_frame = end_frame
+        self.preload = preload
+
+        self.fps_step = int(self.video.get(cv2.CAP_PROP_FPS) / fps)
+
+        self.cache = {}
+        self.previous_id = 0
+        self.max_cache_size = 20
+
+        if self.preload:
+            self.data = []
+            self.data_time = []
+            self.video.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
+            if end_frame == 0:
+                end_frame = int(self.video.get(cv2.CAP_PROP_FRAME_COUNT))
+
+            frame_rgb = []
+            frame_grey = []
+            print(f"Loading video ({end_frame-start_frame} frames)...")
+            valid = True
+            for i in range(start_frame, end_frame, self.fps_step):
+                if i % 1000 == 0:
+                    print(f"Frame {i}...")
+                for j in range(self.fps_step):
+                    valid, frame_rgb = self.video.read()
+                if valid:
+                    frame_rgb, frame_grey = self._process_frame(frame_rgb)
+                    if len(frame_rgb) != 0:
+                        self.data.append(frame_rgb)
+                        self.data_time.append(frame_grey)
+            for i in range(5):
+                self.data_time.append(frame_grey)
+            print(
+                f"Video loaded. Nb of output frames : {int((end_frame-start_frame)/self.fps_step)} \n"
+            )
+
+    def __len__(self):
+        if self.end_frame > self.start_frame:
+            return self.end_frame - self.start_frame
+        else:
+            return int(self.video.get(cv2.CAP_PROP_FRAME_COUNT) / self.fps_step - self.start_frame)
+
+    def __getitem__(self, idx):
+        f0 = self.getFrame(idx)
+        f1 = self.getFrame(idx + 1)
+        f2 = self.getFrame(idx + 2)
+        f3 = self.getFrame(idx + 3)
+        f4 = self.getFrame(idx + 4)
+        f5 = self.getFrame(idx + 5)
+        f6 = self.getFrame(idx + 6)
+        f7 = self.getFrame(idx + 7)
+        f8 = self.getFrame(idx + 8)
+        f9 = self.getFrame(idx + 9)
+        f10 = self.getFrame(idx + 10)
+        f11 = self.getFrame(idx + 11)
+        f12 = self.getFrame(idx + 12)
+        f13 = self.getFrame(idx + 13)
+        f14 = self.getFrame(idx + 14)
+        f15 = self.getFrame(idx + 15)
+
+        fd0 = f1[1] - f0[1]
+        fd1 = f2[1] - f1[1]
+        fd2 = f3[1] - f2[1]
+        fd3 = f4[1] - f3[1]
+        fd4 = f5[1] - f4[1]
+        fd5 = f7[1] - f6[1]
+        fd6 = f9[1] - f8[1]
+        fd7 = f11[1] - f10[1]
+        fd8 = f13[1] - f12[1]
+        fd9 = f15[1] - f14[1]
+        return f0[0], torch.cat((fd0, fd1, fd2, fd3, fd4, fd5, fd6, fd7, fd8, fd9), dim=0)
+
+    def getFrame(self, id):
+        if self.preload:
+            if id < len(self.data):
+                return self.data[id], self.data_time[id]
+            else:
+                return self.getFrame(len(self.data) - 1)
+
+        elif id in self.cache.keys():
+            return self.cache[id]
+
+        else:
+            if id != self.previous_id + 1:
+                self.video.set(cv2.CAP_PROP_POS_FRAMES, (id - 1) * self.fps_step + self.start_frame)
+
+            for i in range(self.fps_step):
+                valid, frame = self.video.read()
+
+            self.previous_id = id
+            if valid:
+                result = self._process_frame(frame)
+
+                self.cache[id] = result
+                if len(self.cache) > self.max_cache_size:
+                    del self.cache[min(self.cache.keys())]
+
+                return result
+            else:
+                return self.getFrame(self.__len__() - 1)
+
+    def getFrameStep(self):
+        return self.fps_step
+
+    def _process_frame(self, frame):
+        frame = cv2.resize(frame, (self.output_width, self.output_height))
+        frame_rgb = torch.permute(torch.FloatTensor(frame / 255), (2, 0, 1))
+
+        frame_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame_grey = torch.FloatTensor(frame_grey[None, :, :] / 255)
+
+        return frame_rgb, frame_grey
