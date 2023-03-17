3eda36bf8579f1bb79736e0343b6d081d1835031
Stache <stache@stache.cat>
2023-03-17 13:55:49 +0100

    Slight refactor

--- Webapp/app.py
+++ Webapp/app.py
@@ -16,27 +16,31 @@ ALLOWED_EXTENSIONS = {"mkv", "mp4", "webm", "avi"}
 DEMO_TRUTH = "truth.csv"
 DEMO_FNAME = "video.webm"
 
+
 def get_locale():
-    print("GET ", request.cookies.get('lang') )
-    if request.args.get('lang') != None:
+    print("GET ", request.cookies.get('lang'))
+    if request.args.get('lang'):
         return request.args.get('lang')
-    if request.cookies.get('lang') != None:
+    if request.cookies.get('lang'):
         return request.cookies.get('lang')
     else:
         return request.accept_languages.best_match(['fr', 'en'])
 
+
 app = Flask(__name__)
 babel = Babel(app, locale_selector=get_locale)
 app.config["UPLOAD_FOLDER"] = os.path.join("static", "inputs")
-app.config["DEMO_FOLDER"]   = os.path.join("static", "inputs", "demo")
+app.config["DEMO_FOLDER"] = os.path.join("static", "inputs", "demo")
 app.config["MAX_CONTENT_PATH"] = 200_000_000
 app.config.from_object("config")
 
 current_ML_thread = None
 current_ML_progress = 0
 
+
 def root_path_join(*args):
     return os.path.join(app.root_path, *args)
+
 
 def clean_upload_folder():
     for f in os.listdir(app.config["UPLOAD_FOLDER"]):
@@ -44,26 +48,32 @@ def clean_upload_folder():
         if os.path.isfile(full_path) and f != ".gitkeep":
             os.remove(full_path)
 
+
 def flash_and_redirect(dest, msg):
     flash(msg)
     return redirect(url_for(dest))
 
+
 def allowed_file(filename):
     return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 def run_ML(path):
     ML.model.predict(path, callback=run_ML_callback)
 
+
 def run_ML_callback(progress):
     global current_ML_progress
-    print("Progress "+str(round(progress*100,2))+"% ...")
+    print(f"Progress {str(round(progress * 100, 2))}%...")
     current_ML_progress = progress
+
 
 @app.after_request
 def after_request(response):
-    if request.args.get('lang') != None:
+    if request.args.get('lang'):
         response.set_cookie('lang', request.args.get('lang'))
     return response
+
 
 @app.route("/uploader", methods=["GET", "POST"])
 def upload_file():
@@ -78,9 +88,8 @@ def upload_file():
 
         if not (file and allowed_file(file.filename)):
             return flash_and_redirect("experience", gettext("Please, use a video in a valid format"))
-            
 
-        if current_ML_thread == None :
+        if not current_ML_thread:
             clean_upload_folder()
             fname = secure_filename(file.filename)
             full_path = root_path_join(app.config["UPLOAD_FOLDER"], fname)
@@ -92,6 +101,7 @@ def upload_file():
             return redirect(url_for("waiting", filename=fname))
         else:
             return redirect(url_for("overloaded"))
+
 
 @app.route("/example")
 def example():
@@ -111,34 +121,42 @@ def example():
 def experience():
     return render_template("pages/experience.html")
 
+
 @app.route("/")
 def home():
     return render_template("pages/home.html")
+
 
 @app.route("/recording")
 def recording():
     return render_template("pages/recording.html")
 
+
 @app.route("/report")
 def report():
     return render_template("pages/report.html")
+
 
 @app.route("/results")
 def results():
     return render_template("pages/results.html")
 
+
 @app.route("/overloaded")
 def overloaded():
     return render_template("pages/overloaded.html")
+
 
 @app.route("/waiting")
 def waiting():
     return render_template("pages/waiting.html")
 
+
 @app.route("/progression")
 def progression():
     global current_ML_progress
-    return {"progression":current_ML_progress}
+    return {"progression": current_ML_progress}
+
 
 @app.route("/markov.png")
 def markov():
@@ -156,12 +174,13 @@ def markov():
 def experience_show():
     if not request.args.get("filename"):
         return flash_and_redirect("experience", gettext("Please, upload a video first"))
-        
+
     filename = os.path.join(app.config["UPLOAD_FOLDER"], request.args.get("filename"))
     if not os.path.isfile(filename):
         return flash_and_redirect("experience", gettext("The file seems to be invalid"))
 
     return render_template("pages/experience-show.html", filename=filename)
+
 
 """
 @app.route("/barcode.png")
@@ -189,15 +208,18 @@ def tenpatterns():
     return Response(output.getvalue(), mimetype="image/png")
 """
 
+
 # Error handlers.
 
 @app.errorhandler(500)
 def internal_error(error):
     return render_template("errors/500.html"), 500
 
+
 @app.errorhandler(404)
 def not_found_error(error):
     return render_template("errors/404.html"), 404
+
 
 if not app.debug:
     file_handler = FileHandler("error.log")
